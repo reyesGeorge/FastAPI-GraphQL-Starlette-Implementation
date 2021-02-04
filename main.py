0f83c6a3a823fa8f2a5c1470585e8e07d95472dc
George Reyes <georgereyes@Georges-MacBook-Pro.local>
2021-02-04 02:49:16 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,115 @@
+from fastapi import FastAPI
+from graphene import ObjectType, List, String, Field, Schema, Mutation
+from graphql.execution.executors.asyncio import AsyncioExecutor
+from starlette.graphql import GraphQLApp
+from schemas import CourseType
+import json
+
+
+
+class Query(ObjectType):
+  course_list = None
+  get_course = Field(List(CourseType), id=String())
+
+  async def resolve_get_course(self, info, id=None):
+    with open("./courses.json") as courses:
+      course_list = json.load(courses)
+    if (id):
+      for course in course_list:
+        if course['id'] == id: return [course]
+    return course_list
+
+class CreateCourse(Mutation):
+  course = Field(CourseType)
+
+  class Arguments:
+    id = String(required=True)
+    title = String(required=True)
+    instructor = String(required=True)
+    publish_date = String()
+
+  async def mutate(self, info, id, title, instructor):
+    with open("./courses.json", "r+") as courses:
+      course_list = json.load(courses)
+
+      for course in course_list:
+        if course['id'] == id:
+          raise Exception('Course with provided id already exists!')
+
+      course_list.append({"id": id, "title": title, "instructor": instructor})
+      courses.seek(0)
+      json.dump(course_list, courses, indent=2)
+    return CreateCourse(course=course_list[-1])
+
+class UpdateCourse(Mutation):
+  course = Field(CourseType)
+
+  class Arguments:
+    id = String(required=True)
+    title = String(required=True)
+    instructor = String(required=True)
+    publish_date = String()
+
+  async def mutate(self, info, id, title, instructor):
+    with open("./courses.json", "r+") as courses:
+      course_list = json.load(courses)
+
+      for course in course_list:
+        if course['id'] == id:
+          indexOfCourse = course_list.index(course)
+          course_list[indexOfCourse] = {"id": id, "title": title, "instructor": instructor}
+          courses.seek(0)
+          courses.truncate()
+
+          json.dump(course_list, courses, indent=2)
+    return UpdateCourse(course=course_list[indexOfCourse])
+
+class Mutation(ObjectType):
+  create_course = CreateCourse.Field()
+  Update_course = UpdateCourse.Field()
+
+app = FastAPI()
+
+
+app.add_route("/", GraphQLApp(
+  schema=Schema(query=Query, mutation=Mutation),
+  executor_class=AsyncioExecutor)
+)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/hey")
+async def urMsg():
+    return {"message": "This is my message"}
+
+
+
+
+
+# {
+#   getCourse(id: "2") {
+#     id
+#     title
+#     instructor
+#     publishDate
+#         }
+# }
+
+
+# mutation {
+#   createCourse(
+#     id: "11" 
+#     title: "Python Lists" 
+#     instructor: "Jane Melody"
+#   ) {
+#     course {
+#       id
+#       title
+#       instructor
+#     }
+#   }
+# } 
